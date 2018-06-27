7b0ea0e92190c2c164b28babbc1613d2477efdce
Andor <agoston.andor@gmail.com>
2018-06-27 19:13:50 +0200

    third

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('The answer is 42.'))
+var port = process.env.PORT || 3000
+
+var logs = []
+
+function buildHTML(html) {
+    html +=
+    `<form method="post" action="/">
+    <input type="text" name="user[name]">
+    <input type="submit" value="Submit">
+    </form>`
+    return html
+}
+
+app.post('/', (req, res) => {
+    if(req.body.user) {
+        var date = new Date()
+        var msg = (date.toGMTString()) + req.body.user.name;
+        logs.push(msg);
+    }
+    var html = ""
+    for (var index in logs) {
+        html += "<br>" + logs[index]
+    }
+    res.send(buildHTML(html))
+})
+
+app.get('/', (req, res) => {
+    var html = "";
+    for(var index in logs){
+        html += "<br>" + logs[index]
+    }
+    res.send(buildHTML(html))
+})
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
